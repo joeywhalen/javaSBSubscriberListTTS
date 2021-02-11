271a493e1ad6c4855ec710f5660b1aa7a1faab2a
Joey Whalen <jtktwhalen@gmail.com>
2021-02-11 09:40:59 -0500

    initial commit

---
+++ src/main/java/com/tts/subscriberlisttts/SubscriberListTtsApplication.java
@@ -0,0 +1,13 @@
+package com.tts.subscriberlisttts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriberListTtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriberListTtsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/subscriberlisttts/SubscriberListTtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.subscriberlisttts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriberListTtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
